0ee202dd77cae97c5afa0a1790b311135adb61f6
snipernoble3 <natelow@rocketmail.com>
2017-03-03 14:15:09 -0800

    Motor Speeds, Testing Motor
    Set up toggles for different motor controllers, also put in the correct
    motor speeds for the current systems. Have not yet moved all functions
    to the correct buttons yet.

--- SteamworksRobot/src/org/usfirst/frc/team5941/robot/Robot.java
+++ SteamworksRobot/src/org/usfirst/frc/team5941/robot/Robot.java
@@ -29,22 +29,93 @@ public class Robot extends IterativeRobot {
 	VictorSP winch = new VictorSP(2);  // PWM 2 - Winch Motor Controller
 	VictorSP intake = new VictorSP(3); // PWM 3 - Ball Intake Motor Controller
 	VictorSP shooter = new VictorSP(4); // PWM 4 - Shooter
-	// PWM 5 - Right Line Sensor
+	// PWM 5 - Right Line Sensor	
 	// PWM 6 - Left Line Sensor
 	// PWM 7 - Shooter Servo
 	// PWM 8 - Door Servo
-	//TalonSRX testStuff = new TalonSRX(9); // PWM 9 - Test/Talon
+	
+	
+	//TESTING
+	TalonSRX testStuff = new TalonSRX(9); // PWM 9 - Test/Talon
+	VictorSP practiceLeftA = new VictorSP(5); //practice left drive motor A
+	VictorSP practiceLeftB = new VictorSP(6); //practice left drive motor B
+	VictorSP practiceRightA = new VictorSP(7); //practice right drive motor A
+	VictorSP practiceRightB = new VictorSP(8); //practice right drive motor B
+	int testMotor = 0;
 	
     XboxController xbox = new XboxController(0); 
 
-	boolean climbingDisabled = true;
+	int climbing = 0;
 	
 	double driverForwardSpeed = 0.5;
 	double driverTurnSpeed = 0.2;
 	
 	boolean goodDriver = false;
 	
-    /**
+	boolean runIntake = true;
+	
+	boolean runAuto = true;
+	Timer t = new Timer();
+    
+	
+	
+	/**
+	 * This function is for 1 run of autonomous
+	 */
+	public void autoCode(String selection){
+		
+		//start timer
+		t.start();
+		
+		if (selection.equals("Middle")){
+			
+			/**
+			 * Currently (for testing):
+			 * -waits 1 second
+			 * -moves at half speed forward for 3 seconds
+			 * -stop motors
+			 */
+			
+			while (t.get() < 1.0){
+				//right.set(0);
+	        	//left.set(0);
+	    		practiceLeftA.set(0);
+	    		practiceLeftB.set(0);
+	    		practiceRightA.set(0);
+	    		practiceRightB.set(0);
+			}
+			while (t.get() < 4.0){
+				//right.set(0.5);
+	        	//left.set(0.5);
+	    		practiceLeftA.set(0.5);
+	    		practiceLeftB.set(0.5);
+	    		practiceRightA.set(0.5);
+	    		practiceRightB.set(0.5);
+			}
+			//right.set(0);
+        	//left.set(0);
+    		practiceLeftA.set(0);
+    		practiceLeftB.set(0);
+    		practiceRightA.set(0);
+    		practiceRightB.set(0);
+		} else if (selection.equals("Left Gear")){
+			
+		} else if (selection.equals("Right Gear")){
+			
+		} else if (selection.equals("Fuel")){
+			//getAlliance stuff
+		} else {
+			//do nothing because for some reason it broke
+		}
+		
+		t.stop();
+	}
+	
+	
+	
+	
+	
+	/**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
@@ -77,7 +148,10 @@ public class Robot extends IterativeRobot {
     public void autonomousPeriodic() {
     	switch(autoSelected) {
     	case customAuto:
-        //Put custom auto code here   
+    		if (runAuto){
+    			autoCode("Middle");
+    			runAuto = false;
+    		}
             break;
     	case defaultAuto:
     	default:
@@ -91,6 +165,13 @@ public class Robot extends IterativeRobot {
      */
     public void teleopPeriodic() {
 		
+    	/*
+    	 * if (runIntake)
+    	 *     intake.set(0.3);
+    	 * else
+    	 *     intake.set(0);
+    	 */
+    	
 		//Driving
     	double rightAxis = xbox.getRawAxis(5), leftAxis = -xbox.getRawAxis(1); 
 		
@@ -119,14 +200,22 @@ public class Robot extends IterativeRobot {
 		//If turning motors at 33% speed, else at 75% speed
     	if((rightAxis > 0 && leftAxis > 0) || (rightAxis < 0 && leftAxis < 0)){
     		
-			right.set(rightAxis*driverTurnSpeed);
-    		left.set(leftAxis*driverTurnSpeed);
-			
+			//right.set(rightAxis*driverTurnSpeed);
+    		//left.set(leftAxis*driverTurnSpeed);
+			practiceLeftA.set(leftAxis*driverTurnSpeed);
+			practiceLeftB.set(leftAxis*driverTurnSpeed);
+			practiceRightA.set(rightAxis*driverTurnSpeed);
+			practiceRightB.set(rightAxis*driverTurnSpeed);
+    		
     	} else {
     		
-			right.set(rightAxis*driverForwardSpeed);
-        	left.set(leftAxis*driverForwardSpeed);
-    	
+			//right.set(rightAxis*driverForwardSpeed);
+        	//left.set(leftAxis*driverForwardSpeed);
+    		practiceLeftA.set(leftAxis*driverForwardSpeed);
+    		practiceLeftB.set(leftAxis*driverForwardSpeed);
+    		practiceRightA.set(rightAxis*driverForwardSpeed);
+    		practiceRightB.set(rightAxis*driverForwardSpeed);
+    		
 		}
 		
     	
@@ -134,26 +223,43 @@ public class Robot extends IterativeRobot {
 		//Climbing
 		if(xbox.getRawButton(8)){ //Start button
 			
-			if (climbingDisabled == true){
+			if (climbing == 2){
 				
-				climbingDisabled = false;
+				climbing = 0;
 				
-			} else if (climbingDisabled == false){
+			} else {
 				
-				climbingDisabled = true;
+				climbing++;
 				
 			}
 			
 		}
 		
 		
-		if(!climbingDisabled && xbox.getRawButton(5)){ //Left Bumper
+		if(climbing != 0 && xbox.getRawButton(5)){ //Left Bumper
 			
-			winch.set(0.5);
+			if (climbing == 1){
+				
+				winch.set(1);
+				
+				if (testMotor == 0)
+					testStuff.set(1);
+			
+			} else if (climbing == 2){
+				
+				winch.set(-1);
+				
+				if (testMotor == 0)
+					testStuff.set(-1);
+			
+			}
 		
 		} else {
 		
 			winch.set(0);
+			
+			if (testMotor == 0)
+				testStuff.set(0);
 		
 		}
 		
@@ -162,11 +268,17 @@ public class Robot extends IterativeRobot {
 		//Ball Intake
 		if(xbox.getRawAxis(2) > 0.2){ //Left Trigger
 			
-			intake.set(-1);
+			intake.set(0.3);
+			
+			if (testMotor == 1)
+				testStuff.set(0.3);
 		
 		} else {
 		
 			intake.set(0);
+			
+			if (testMotor == 1)
+				testStuff.set(0);
 		
 		}
 		
@@ -175,27 +287,37 @@ public class Robot extends IterativeRobot {
 		//Shooter
 		if(xbox.getRawAxis(3) > 0.2){ //Right Trigger
 		
-			shooter.set(0.5);
+			shooter.set(0.7);
+			
+			if (testMotor == 2)
+				testStuff.set(0.7);
 		
 		} else {
 		
 			shooter.set(0);
-		
-		}
-		
-		
-		/*
-		//Test Stuff
-		if(xbox.getRawButton(5)){ // LeftBumper
 			
-			testStuff.set(0.24);
-		
-		} else {
-		
-			testStuff.set(0);
+			if (testMotor == 2)
+				testStuff.set(0);
 		
 		}
-		*/
+		
+		
+		
+		//Test Stuff
+		if(xbox.getRawButton(3)){ // X Button
+			
+			if (testMotor > 1){
+				
+				testMotor = 0;
+				
+			} else {
+				
+				testMotor++;
+				
+			}
+		
+		}
+		
 		
     }
     
