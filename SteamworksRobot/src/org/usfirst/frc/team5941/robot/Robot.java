fb1ed748927e5aa39e216ca05239c6468596b077
snipernoble3 <natelow@rocketmail.com>
2017-03-17 15:01:29 -0700

    Right Auto, Buttons for first comp
    The dashboard has been set up, driving is set, the auto code for the
    center needs to be tested with the main robot, but the right side is
    working. Buttons have been set up/changed since last code update.

--- SteamworksRobot/src/org/usfirst/frc/team5941/robot/Robot.java
+++ SteamworksRobot/src/org/usfirst/frc/team5941/robot/Robot.java
@@ -10,6 +10,9 @@ import edu.wpi.first.wpilibj.TalonSRX;
 //import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.CameraServer;
+//import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.Servo;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -19,22 +22,23 @@ import edu.wpi.first.wpilibj.CameraServer;
  */
 public class Robot extends IterativeRobot {
     final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
+    final String middleAuto = "Middle";
+    final String leftAuto = "Left";
+    final String rightAuto = "Right";
+    final String fuelAuto = "Fuel";
     String autoSelected;
     SendableChooser chooser;
 
 
 	VictorSP right = new VictorSP(0);  //Right Side Drive
 	VictorSP left = new VictorSP(1);   //Left Side Drive
-	VictorSP winch = new VictorSP(2);  // PWM 2 - Winch Motor Controller
-	VictorSP intake = new VictorSP(3); // PWM 3 - Ball Intake Motor Controller
-	VictorSP shooter = new VictorSP(4); // PWM 4 - Shooter
-	// PWM 5 - Right Line Sensor	
-	// PWM 6 - Left Line Sensor
-	// PWM 7 - Shooter Servo
-	// PWM 8 - Door Servo
+	VictorSP winchA = new VictorSP(2);  // PWM 2 - Winch Motor Controller
+	VictorSP winchB = new VictorSP(3); // PWM 3 - Winch Motor Controller
+	VictorSP intake = new VictorSP(4); // PWM 4 - Intake
+	TalonSRX shooter = new TalonSRX(5); // PWM 5 - Shooter	
+	Servo ballStop = new Servo(6); //PWM 6 - Ball Stop
 	
-	
+	/*
 	//TESTING
 	TalonSRX testStuff = new TalonSRX(9); // PWM 9 - Test/Talon
 	VictorSP practiceLeftA = new VictorSP(5); //practice left drive motor A
@@ -43,9 +47,15 @@ public class Robot extends IterativeRobot {
 	VictorSP practiceRightB = new VictorSP(8); //practice right drive motor B
 	int testMotor = 0;
 	
+	//PWM lineSensor = new PWM(4);
+	//END TEST STUFF
+	*/
+	
+	
     XboxController xbox = new XboxController(0); 
 
 	int climbing = 0;
+	boolean climbingReverseEnabled = false;
 	
 	double driverForwardSpeed = 0.5;
 	double driverTurnSpeed = 0.2;
@@ -53,6 +63,8 @@ public class Robot extends IterativeRobot {
 	boolean goodDriver = false;
 	
 	boolean runIntake = true;
+	boolean shooterReady = false;
+	boolean charging = false;
 	
 	boolean runAuto = true;
 	Timer t = new Timer();
@@ -72,35 +84,39 @@ public class Robot extends IterativeRobot {
 			/**
 			 * Currently (for testing):
 			 * -waits 1 second
-			 * -moves at half speed forward for 3 seconds
+			 * -moves at 0.33
 			 * -stop motors
 			 */
 			
 			while (t.get() < 1.0){
-				//right.set(0);
-	        	//left.set(0);
-	    		practiceLeftA.set(0);
-	    		practiceLeftB.set(0);
-	    		practiceRightA.set(0);
-	    		practiceRightB.set(0);
+				left.set(0);
+	        	right.set(0);
+	    		//practiceLeftA.set(0);
+	    		//practiceLeftB.set(0);
+	    		//practiceRightA.set(0);
+	    		//practiceRightB.set(0);
 			}
-			while (t.get() < 4.0){ //Reverse for 3 seconds (for gear delivery)
-				//right.set(-0.5);
-	        	//left.set(0.5);
-	    		practiceLeftA.set(0.5); //Since Left is Negative=Forward, Positive is Reverse
-	    		practiceLeftB.set(0.5);
-	    		practiceRightA.set(-0.5); //Since Right is Positive=Forward, Negative is Reverse
-	    		practiceRightB.set(-0.5);
+			while (t.get() < 2.8){ //Backward for 1.5 seconds (for gear delivery) 
+				/*
+				 * for 0.5 speed (0.46 right), 1.75 sec
+				 * for 0.35 speed (0.33 right), 2.5 sec
+				 */
+	        	left.set(-0.35);
+	        	right.set(0.33);
+	    		//practiceLeftA.set(0.35); 
+	    		//practiceLeftB.set(0.35);
+	    		//practiceRightA.set(-0.33); 
+	    		//practiceRightB.set(-0.33);
 			}
-			//right.set(0);
-        	//left.set(0);
-    		practiceLeftA.set(0);
-    		practiceLeftB.set(0);
-    		practiceRightA.set(0);
-    		practiceRightB.set(0);
-		} else if (selection.equals("Left Turn")){
+			left.set(0);
+        	right.set(0);
+    		//practiceLeftA.set(0);
+    		//practiceLeftB.set(0);
+    		//practiceRightA.set(0);
+    		//practiceRightB.set(0);
+		} else if (selection.equals("Left")){
 			//60 degree turns
-			//TEST CODE
+			//TEST CODE for Left Turn, not Left Side Start
 			/**
 			 * Currently (for testing):
 			 * -waits 1 second
@@ -109,28 +125,60 @@ public class Robot extends IterativeRobot {
 			 */
 			
 			while (t.get() < 1.0){
-				//right.set(0);
-	        	//left.set(0);
-	    		practiceLeftA.set(0);
-	    		practiceLeftB.set(0);
-	    		practiceRightA.set(0);
-	    		practiceRightB.set(0);
+				left.set(0);
+	        	right.set(0);
+	    		//practiceLeftA.set(0);
+	    		//practiceLeftB.set(0);
+	    		//practiceRightA.set(0);
+	    		//practiceRightB.set(0);
 			}
-			while (t.get() < 2.0){ //Spin at half speed for 1 second
-				//right.set(-0.5);
-	        	//left.set(-0.5);
-	    		practiceLeftA.set(-0.5); //Since Left is Negative=Forward, Positive is Reverse
-	    		practiceLeftB.set(-0.5);
-	    		practiceRightA.set(-0.5); //Since Right is Positive=Forward, Negative is Reverse
-	    		practiceRightB.set(-0.5);
+			while (t.get() < 1.25){ //Spin counter clockwise at 0.35/0.33 speed for 0.25 seconds
+	        	left.set(-0.35);
+	        	right.set(-0.33);
+	    		//practiceLeftA.set(-0.35); //Since Left is Negative=Forward, Positive is Reverse
+	    		//practiceLeftB.set(-0.35);
+	    		//practiceRightA.set(-0.33); //Since Right is Positive=Forward, Negative is Reverse
+	    		//practiceRightB.set(-0.33);
 			}
-			//right.set(0);
-        	//left.set(0);
-    		practiceLeftA.set(0);
-    		practiceLeftB.set(0);
-    		practiceRightA.set(0);
-    		practiceRightB.set(0);
-		} else if (selection.equals("Right Gear")){
+			left.set(0);
+        	right.set(0);
+    		//practiceLeftA.set(0);
+    		//practiceLeftB.set(0);
+    		//practiceRightA.set(0);
+    		//practiceRightB.set(0);
+		} else if (selection.equals("Right")){
+			while(t.get() < 1.0){
+				left.set(0);
+				right.set(0);
+			}
+			while(t.get() < 2.9){
+				left.set(-0.34);
+				right.set(0.33);
+			}
+			while(t.get() < 3.0){
+				left.set(0);
+				right.set(0);
+			}
+			while(t.get() < 3.3){
+				left.set(-0.35);
+				right.set(-0.33);
+			}
+			while(t.get() < 3.5){
+				left.set(0);
+				right.set(0);
+			}
+			while(t.get() < 4.25){
+				left.set(-0.35);
+				right.set(0.33);
+			}
+			left.set(0);
+			right.set(0);
+			
+			/*
+			 * turn a bit more, right side too fast on straight, short distance too short
+			 */
+			
+			
 			
 		} else if (selection.equals("Fuel")){
 			//getAlliance stuff
@@ -139,6 +187,7 @@ public class Robot extends IterativeRobot {
 		}
 		
 		t.stop();
+		t.reset();
 	}
 	
 	
@@ -152,9 +201,12 @@ public class Robot extends IterativeRobot {
     public void robotInit() {
         chooser = new SendableChooser();
         chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("My Auto", customAuto);
+        chooser.addObject("My Auto", middleAuto);
+        chooser.addObject("Left", leftAuto);
+        chooser.addObject("Right", rightAuto);
+        chooser.addObject("Fuel", fuelAuto);
         SmartDashboard.putData("Auto choices", chooser);
-        //CameraServer.getInstance().startAutomaticCapture(0);
+        CameraServer.getInstance().startAutomaticCapture(0);
     }
     
 	/**
@@ -177,9 +229,27 @@ public class Robot extends IterativeRobot {
      */
     public void autonomousPeriodic() {
     	switch(autoSelected) {
-    	case customAuto:
+    	case middleAuto:
     		if (runAuto){
     			autoCode("Middle");
+    			runAuto = false;
+    		}
+            break;
+    	case leftAuto:
+    		if (runAuto){
+    			autoCode("Left");
+    			runAuto = false;
+    		}
+            break;
+    	case rightAuto:
+    		if (runAuto){
+    			autoCode("Right");
+    			runAuto = false;
+    		}
+            break;
+    	case fuelAuto:
+    		if (runAuto){
+    			autoCode("Fuel");
     			runAuto = false;
     		}
             break;
@@ -201,7 +271,12 @@ public class Robot extends IterativeRobot {
     	 * else
     	 *     intake.set(0);
     	 */
-    	
+    	/*
+    	if (lineSensor.getRaw() > 650)
+    		testStuff.set(0.5);
+    	else
+    		testStuff.set(0);
+    	*/
 		//Driving
     	double rightAxis = xbox.getRawAxis(5), leftAxis = -xbox.getRawAxis(1); 
 		
@@ -230,26 +305,87 @@ public class Robot extends IterativeRobot {
 		//If turning motors at 33% speed, else at 75% speed
     	if((rightAxis > 0 && leftAxis > 0) || (rightAxis < 0 && leftAxis < 0)){
     		
-			//right.set(rightAxis*driverTurnSpeed);
-    		//left.set(leftAxis*driverTurnSpeed);
-			practiceLeftA.set(leftAxis*driverTurnSpeed);
-			practiceLeftB.set(leftAxis*driverTurnSpeed);
-			practiceRightA.set(rightAxis*driverTurnSpeed);
-			practiceRightB.set(rightAxis*driverTurnSpeed);
+			right.set(rightAxis*driverTurnSpeed);
+    		left.set(leftAxis*driverTurnSpeed);
+			//practiceLeftA.set(leftAxis*driverTurnSpeed);
+			//practiceLeftB.set(leftAxis*driverTurnSpeed);
+			//practiceRightA.set(rightAxis*driverTurnSpeed);
+			//practiceRightB.set(rightAxis*driverTurnSpeed);
     		
     	} else {
     		
-			//right.set(rightAxis*driverForwardSpeed);
-        	//left.set(leftAxis*driverForwardSpeed);
-    		practiceLeftA.set(leftAxis*driverForwardSpeed);
-    		practiceLeftB.set(leftAxis*driverForwardSpeed);
-    		practiceRightA.set(rightAxis*driverForwardSpeed);
-    		practiceRightB.set(rightAxis*driverForwardSpeed);
+			right.set(rightAxis*driverForwardSpeed);
+        	left.set(leftAxis*driverForwardSpeed);
+    		//practiceLeftA.set(leftAxis*driverForwardSpeed);
+    		//practiceLeftB.set(leftAxis*driverForwardSpeed);
+    		//practiceRightA.set(rightAxis*driverForwardSpeed);
+    		//practiceRightB.set(rightAxis*driverForwardSpeed);
     		
 		}
 		
     	
+
+    	//Weekend 1
+    	
+    	//Climbing
+    	
+    	if (!climbingReverseEnabled && xbox.getRawButton(8) && xbox.getRawButton(5) && xbox.getRawButton(6)) { //if start + left bumper + right bumper, unlock winch reverse
+    		climbingReverseEnabled = true; 
+    	} //if start + left bumper + right bumper, unlock winch reverse
+    	
+    	if (climbingReverseEnabled && (xbox.getRawAxis(2) > 0.05)){ //if winch can reverse and left trigger, reverse at speed of press
+    		winchA.set(-(xbox.getRawAxis(2))*0.1);
+    		winchB.set(-(xbox.getRawAxis(2))*0.1);
+    	} else if (xbox.getRawAxis(3) > 0.05) { //else if right trigger, climb (forward) at speed of press
+    		winchA.set(xbox.getRawAxis(3)*0.5);
+    		winchB.set(xbox.getRawAxis(3)*0.5);
+    	} else { //else stop
+    		winchA.set(0);
+    		winchB.set(0);
+    	}
 		
+    	
+    	//Intake
+    	if (xbox.getRawButton(5)) //Left Bumper (?)
+    		intake.set(0.3);
+    	else
+    		intake.set(0);
+    	
+    	
+    	
+    	//Shooter
+    	if(xbox.getRawButton(6) && !charging) {//Right Bumper (?)
+    		if(!shooterReady){
+    			charging = true;
+    			t.reset();
+    			shooter.set(0.75);
+    			t.start();
+    		} else if (shooterReady){
+    			ballStop.setAngle(0); //Lower servo to stop balls
+    			shooter.set(0);
+    			shooterReady = false;
+    		}
+    	}
+    	
+    	if(!shooterReady && t.get() > 1.5){
+    		shooterReady = true;
+    		t.stop();
+    		t.reset();
+    	}
+    	
+    	if(shooterReady){
+    		ballStop.setAngle(90); //Raise servo to let balls into shooter
+    		charging = false;
+    	}
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+		/* OLD CODE
 		//Climbing
 		if(xbox.getRawButton(8)){ //Start button
 			
@@ -317,22 +453,22 @@ public class Robot extends IterativeRobot {
 		//Shooter
 		if(xbox.getRawAxis(3) > 0.2){ //Right Trigger
 		
-			shooter.set(0.7);
+			//shooter.set(0.7);
 			
 			if (testMotor == 2)
 				testStuff.set(0.7);
 		
 		} else {
 		
-			shooter.set(0);
+			//shooter.set(0);
 			
 			if (testMotor == 2)
 				testStuff.set(0);
 		
 		}
-		
-		
-		
+		*/
+    	
+    	/*
 		//Test Stuff
 		if(xbox.getRawButton(3)){ // X Button
 			
@@ -347,7 +483,7 @@ public class Robot extends IterativeRobot {
 			}
 		
 		}
-		
+		*/
 		
     }
     
